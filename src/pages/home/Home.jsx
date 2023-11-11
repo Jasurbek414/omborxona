e2c66b193d7737e542491ae50f1c4bafc421d1de
Jasurbek414 <muminovjasurbek12@gmail.com>
2023-11-11 13:17:02 +0500

    add chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -7,31 +7,31 @@ import {
   LinearScale,
   PointElement
 } from "chart.js";
-import "./Home.css"
-
+import "./Home.css";
 
 ChartJS.register(
   LineElement,
   CategoryScale,
   LinearScale,
   PointElement
-)
+);
 
 function Home() {
   const data = {
-    label:["May 12","May 13","May 14","May 15","May 16","May 17",],
-    datasets:[{
-      data:[8, 7.8, 6, 7, 5, 6],
-      backgroundColor:"transparent",
+    labels: ["May 12", "May 13", "May 14", "May 15", "May 16", "May 17"],
+    datasets: [{
+      data: [8, 7.8, 6, 7, 5, 6],
+      backgroundColor: "transparent",
       borderColor: "#f26c6d",
     }]
   };
-  const option = {};
+  const options = {};
+  
   return (
-    <div className='chart-cont overflow-hidden' style={{width: "100%"}}>
-      <Line style={{"margin-left":" 5px"}} data = {data} option = {option}></Line>
+    <div className='chart-cont overflow-hidden' style={{ width: "100%" }}>
+      <Line styles={{ marginLeft: "5px" }} data={data} options={options}></Line>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
